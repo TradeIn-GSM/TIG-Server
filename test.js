e289b43adf94b5d832ad7308699577ab491a21e7
kyh <61466801+1q2f3d@users.noreply.github.com>
2021-01-12 11:05:10 +0900

    Delete test.js

--- test.js
+++
@@ -1,7 +0,0 @@
-var GPIO = require('onoff').Gpio;
-var Coin = new GPIO(4, 'in', 'falling', { debounceTimeout : 50 });
-
-Coin.watch((err, value) => {
-    if (err) throw err;
-    console.log('A');
-});
